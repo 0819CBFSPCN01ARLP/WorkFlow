b8a1e747dcebcbed120aeb60b2cece4dd62a2179
Juliana <juli.franchino@hotmail.com>
2019-12-19 19:39:54 -0300

    Proyecto completo

--- proyecto-integrador/delete-post.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Script para eliminar posts */
-require_once('includes/session-pdo.php');
-$delete_post = $db->prepare("DELETE FROM post WHERE id = :id");
-$delete_post->execute([":id"=>$_GET["postId"]]);
-
-header('Location: ' . $_SERVER['HTTP_REFERER']);
-?>
--- proyecto-integrador/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("includes/session-pdo.php");
-session_destroy();
-header('Location: login.php');
-?>
